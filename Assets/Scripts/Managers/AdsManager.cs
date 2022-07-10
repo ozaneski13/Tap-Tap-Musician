34f80c8532bb950bf4350477ed3a4efe2ea0dcb6
Ozan Eşki <34617144+ozaneski13@users.noreply.github.com>
2022-07-10 23:31:50 +0300

    Ads manager added but not working for now.

---
+++ Assets/Scripts/Managers/AdsManager.cs
@@ -0,0 +1,134 @@
+/*using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+#if UNITY_IOS
+    string gameId = "4834882";
+#else
+    string gameId = "4834883";//"3003911";//"4834883";
+#endif
+
+    private const string BANNER_PLACEMENT = "banner";
+    private const string VIDEO_PLACEMENT = "video";
+    private const string REWARDED_VIDEO_PLACEMENT = "rewardedVideo";
+
+    [SerializeField] private BannerPosition bannerPosition = BannerPosition.BOTTOM_CENTER;
+
+    private bool testMode = false;
+    private bool showBanner = false;
+
+    //utility wrappers for debuglog
+    public delegate void DebugEvent(string msg);
+    public static event DebugEvent OnDebugLog;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        if (Advertisement.isSupported)
+        {
+            DebugLog(Application.platform + " supported by Advertisement");
+        }
+        Advertisement.Initialize(gameId, testMode, this);
+
+        LoadNonRewardedAd();
+    }
+
+    public void ToggleBanner()
+    {
+        showBanner = !showBanner;
+
+        if (showBanner)
+        {
+            Advertisement.Banner.SetPosition(bannerPosition);
+            Advertisement.Banner.Show(BANNER_PLACEMENT);
+        }
+        else
+        {
+            Advertisement.Banner.Hide(false);
+        }
+    }
+
+    public void LoadRewardedAd()
+    {
+        Advertisement.Load(REWARDED_VIDEO_PLACEMENT, this);
+    }
+
+    public void ShowRewardedAd()
+    {
+        Advertisement.Show(REWARDED_VIDEO_PLACEMENT, this);
+    }
+
+    public void LoadNonRewardedAd()
+    {
+        Advertisement.Load(VIDEO_PLACEMENT, this);
+    }
+
+    public void ShowNonRewardedAd()
+    {
+        Advertisement.Show(VIDEO_PLACEMENT, this);
+    }
+
+    #region Interface Implementations
+    public void OnInitializationComplete()
+    {
+        DebugLog("Init Success");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        DebugLog($"Init Failed: [{error}]: {message}");
+    }
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        DebugLog($"Load Success: {placementId}");
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        DebugLog($"Load Failed: [{error}:{placementId}] {message}");
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        DebugLog($"OnUnityAdsShowFailure: [{error}]: {message}");
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        DebugLog($"OnUnityAdsShowStart: {placementId}");
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        DebugLog($"OnUnityAdsShowClick: {placementId}");
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        DebugLog($"OnUnityAdsShowComplete: [{showCompletionState}]: {placementId}");
+    }
+    #endregion
+
+    public void OnGameIDFieldChanged(string newInput)
+    {
+        gameId = newInput;
+    }
+
+    public void ToggleTestMode(bool isOn)
+    {
+        testMode = isOn;
+    }
+
+    //wrapper around debug.log to allow broadcasting log strings to the UI
+    void DebugLog(string msg)
+    {
+        OnDebugLog?.Invoke(msg);
+        Debug.Log(msg);
+    }
+}*/
